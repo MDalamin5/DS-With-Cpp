a7204f2c32b5f81f613d799f7754c57b142214b2
MDalamin5 <mdal.amin5@northsouth.edu>
2023-02-17 23:32:41 +0600

    LL Update

--- Week-4/Module-13/LL_insert_after_a_value/main.cpp
+++ Week-4/Module-13/LL_insert_after_a_value/main.cpp
@@ -1,6 +1,83 @@
 #include<bits/stdc++.h>
 using namespace std;
+class node
+{
+    public:
+    int data;
+    node *next;
+};
+class LinkedList
+{
+    public:
+    node *head;
+    LinkedList()
+    {
+        head=NULL;
+    }
+    node *createAnewNode(int data)
+    {
+        node *newNode = new node;
+        newNode->data=data;
+        newNode->next=NULL;
+        return newNode;
+    }
+    void inserAtHead(int data)
+    {
+        node *a = createAnewNode(data);
+        if(head==NULL)
+        {
+            head=a;
+        }
+        else
+        {
+            a->next=head;
+            head=a;
+        }
+    }
+    void travers()
+    {
+        node *a = head;
+        while(a!=NULL)
+        {
+            cout<<a->data<<" ";;
+            a=a->next;
+        }
+        cout<<endl;
+    }
+    void addDataAfterAvalue(int sp_value,int data)
+    {
+        node *a=head;
+        
+        while(a!=NULL)
+        {
+            if(a->data==sp_value)
+            {
+                break;
+            }
+            a=a->next;
+        }
+       if(a!=NULL)
+       {
+         node *newB = createAnewNode(data);
+        newB->next=a->next;
+        a->next=newB;
+       }
+       else
+       {
+        cout<<"YOUr data not found!"<<"\n";
+        return;
+       }
+    }
+};
 int main()
 {
-    
+    LinkedList l;
+    l.inserAtHead(10);
+    l.inserAtHead(23);
+    l.inserAtHead(25);
+    l.inserAtHead(26);
+    l.travers();
+    l.addDataAfterAvalue(45,99);
+    l.travers();
+   
 }
---
+++ Week-4/Module-13/LL_insert_after_a_value/main2.cpp
@@ -0,0 +1,191 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node* next;
+};
+
+class LinkList
+{
+    public:
+    node *head;
+    int size;
+    LinkList()
+    {
+        head=NULL;
+        size=0;
+    }
+
+    node *createNewNode(int data)
+    {
+        node *newNode = new node;
+        newNode->data=data;
+        newNode->next=NULL;
+        return newNode;
+    }
+    void insertAtHead(int data)
+    {
+        size++;
+        node *a = createNewNode(data);
+        if(head==NULL)
+        {
+            head=a;
+        }
+        else
+        {
+            a->next=head;
+            head=a;
+        }
+    }
+    void travers()
+    {
+        node *a=head;
+        while(a!=NULL)
+        {
+            cout<<a->data<<" ";
+            a=a->next;
+        }
+    }
+    int searchValue(int target)
+    {
+       
+        node *a= head;
+        int index=0;
+        while(a!=NULL)
+        {
+            if(a->data==target)
+            {
+                return index;
+            }
+            a=a->next;
+            index++;
+        }
+        return -1;
+    }
+    int getSize()
+    {
+        //now implements the code to time complexity O(1)
+        return size;
+        //this code work O(n)
+        /*
+        int sz=0;
+        node *a = head;
+        while(a!=NULL)
+        {
+            sz++;
+            a=a->next;
+        }
+        return sz;
+        */
+        
+    }
+    void insertAtAnyIndex(int indx,int value)
+    {
+        if(indx<0 || indx>size)
+        {
+            return;
+        }
+        if(indx==0)
+        {
+            insertAtHead(value);
+        }
+        size++;
+        node *a = head;
+        int current_i=0;
+        while(current_i!=indx-1)
+        {
+            a=a->next;
+            current_i++;
+        }
+        node *newNode= createNewNode(value);
+        newNode->next=a->next;
+        a->next=newNode;
+
+    }
+    void deletAtHead()
+    {
+        if(head==NULL)
+        {
+            cout<<"Notheing to delete\n";
+            return;
+        }
+        size--;
+        node *a = head;
+        head=a->next;
+        delete a;
+    }
+    void deletAtAnyIndex(int index)
+    {
+        if(index>size-1)
+        {
+            cout<<"You are entering wrong index!!"<<endl;
+            return;
+        }
+        if(index==0)
+        {
+            deletAtHead();
+        }
+        size--;
+        node *a = head;
+        int current_i=0;
+        while(current_i!=index)
+        {
+            a=a->next;
+            current_i++;
+        }
+        node *b=a->next;
+        a->next=b->next;
+        delete b;
+    }
+
+    void addDataAfterAvalue(int sp_value,int data)
+    {
+        node *a=head;
+        
+        while(a!=NULL)
+        {
+            if(a->data==sp_value)
+            {
+                break;
+            }
+            a=a->next;
+        }
+       if(a!=NULL)
+       {
+         node *newB = createNewNode(data);
+        newB->next=a->next;
+        a->next=newB;
+       }
+       else
+       {
+        cout<<"YOUr data not found!"<<"\n";
+        return;
+       }
+    }
+};
+int main()
+{
+    LinkList l;
+    l.insertAtHead(10);
+    l.insertAtHead(50);
+    l.insertAtHead(60);
+    l.insertAtHead(70);
+    l.insertAtHead(80);
+    //l.travers();
+    l.insertAtAnyIndex(3,22);
+    l.travers();
+    cout<<endl;
+    l.deletAtHead();
+    l.travers();
+    cout<<endl;
+    l.deletAtAnyIndex(2);
+    l.addDataAfterAvalue(70,99);
+    l.travers();
+    cout<<endl;
+    cout<<l.getSize();
+    //cout<<l.searchValue(10)<<" no index found";
+    
+
+}
