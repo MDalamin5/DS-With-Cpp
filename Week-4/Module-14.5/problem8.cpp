eae2c129463c8eec08d2d2d1e46029743bbb94b3
MDalamin5 <mdal.amin5@northsouth.edu>
2023-02-26 19:35:16 +0600

    Practice problem

---
+++ Week-4/Module-14.5/problem8.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node
+{
+    public:
+    node *pre;
+    int data;
+    node *next;
+};
+int main()
+{
+    cout<<"code is Cooking......";
+    return 0;
+}
