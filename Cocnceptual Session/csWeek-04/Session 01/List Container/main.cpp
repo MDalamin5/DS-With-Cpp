69763ad1b1af634bd54a1d4e6da77efb64d4bd3b
MDalamin5 <mdal.amin5@northsouth.edu>
2023-02-28 22:02:58 +0600

    Codeforce

--- Cocnceptual Session/csWeek-04/Session 01/List Container/main.cpp
+++ Cocnceptual Session/csWeek-04/Session 01/List Container/main.cpp
@@ -4,9 +4,15 @@ int main()
 {
     list<int>l;
     l.push_back(10);
-    l.push_back(12);
+    l.push_back(72);
     l.push_front(30);
     for(auto it = l.begin();it!=l.end();it++)
+    {
+        cout<<*it<<" ";
+    }
+   l.sort();
+   cout<<endl;
+   for(auto it = l.begin();it!=l.end();it++)
     {
         cout<<*it<<" ";
     }
