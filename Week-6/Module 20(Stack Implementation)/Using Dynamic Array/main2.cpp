0df23217de117319a542900a8dc5845169f78229
MDalamin5 <mdal.amin5@northsouth.edu>
2023-03-10 00:31:22 +0600

    stack practice

--- Week-6/Module 20(Stack Implementation)/Using Dynamic Array/main2.cpp
+++ Week-6/Module 20(Stack Implementation)/Using Dynamic Array/main2.cpp
@@ -4,77 +4,77 @@ class Stack
 {
     public:
     int *a;
-    int stack_size;
     int arr_cap;
+    int stack_size;
+
     Stack()
     {
         stack_size=0;
         arr_cap=1;
-        a= new int[1];
+        a= new int[arr_cap];
     }
-    void increseSize()
+    void incriseStackSize()
     {
-        int *temp;
-        temp=new int[arr_cap*2];
+        int *temp = new int [arr_cap*2];
         for(int i=0;i<arr_cap;i++)
-        {
-            temp[i]=a[i];
-        }
+        temp[i]=a[i];
+
         swap(a,temp);
         delete[] temp;
         arr_cap *=2;
-
     }
-    //void push add elements in stack
     void push(int data)
     {
-        stack_size+= 1;
+        stack_size +=1;
         if(stack_size>arr_cap)
         {
-           increseSize();
+            incriseStackSize();
         }
         a[stack_size-1]=data;
     }
-
-    //delete top most elements
     void pop()
     {
         if(stack_size==0)
         {
-            cout<<"Stsck is Empty!";
+            cout<<"stack is Empty";
             return;
         }
         a[stack_size-1]=0;
-        stack_size--;
+        stack_size--; 
     }
-    //return top elements 
+
     int top()
     {
         if(stack_size==0)
         {
-            cout<<"Stsck is Empty!"<<endl;
+            cout<<"Stack Is Empty";
             return -1;
         }
         return a[stack_size-1];
-
     }
+    int StackSize()
+    {
+        return stack_size;
+    }
+
 };
 int main()
 {
-   Stack st;
-   for(int i=0;i<5;i++)
-   {
-    st.push(i+1);
-   }
-   cout<<st.top()<<endl;
-   st.pop();
-   cout<<st.top()<<endl;
-    st.pop();
-   cout<<st.top()<<endl;
-    st.pop();
-   cout<<st.top()<<endl;
-    st.pop();
-   cout<<st.top()<<endl;
-    st.pop();
-   cout<<st.top()<<endl;
+  Stack stk;
+  for(int i=0;i<5;i++)
+  {
+    //stk.push(rand()%10);
+    stk.push(i+1);
+  }
+  cout<<stk.top()<<endl;
+   stk.pop();
+   cout<<stk.top()<<endl;
+    stk.pop();
+   cout<<stk.top()<<endl;
+    stk.pop();
+   cout<<stk.top()<<endl;
+    stk.pop();
+   cout<<stk.top()<<endl;
+    stk.pop();
+   cout<<stk.top()<<endl;
 }
