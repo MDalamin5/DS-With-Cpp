73e0f61ab7509bb357bcd9f1f184cabc7aa46841
MDalamin5 <mdal.amin5@northsouth.edu>
2023-03-12 19:43:18 +0600

    CC session

--- Cocnceptual Session/csWeek-04/Session 01/List Container/main.cpp
+++ Cocnceptual Session/csWeek-04/Session 01/List Container/main.cpp
@@ -3,16 +3,16 @@ using namespace std;
 int main()
 {
     list<int>l;
-    l.push_back(10);
-    l.push_back(72);
-    l.push_front(30);
-    for(auto it = l.begin();it!=l.end();it++)
+    for(int i=0;i<5;i++)
+    l.push_back(rand()%100);
+
+    for(auto it=l.begin(); it!= l.end();it++)
     {
         cout<<*it<<" ";
     }
-   l.sort();
-   cout<<endl;
-   for(auto it = l.begin();it!=l.end();it++)
+    l.sort();
+    cout<<endl;
+    for(auto it=l.begin(); it!= l.end();it++)
     {
         cout<<*it<<" ";
     }
