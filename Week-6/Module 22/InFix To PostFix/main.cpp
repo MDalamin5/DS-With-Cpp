d915fd93282ba2a4707576f029023a576d24d84e
MDalamin5 <mdal.amin5@northsouth.edu>
2023-03-21 00:49:59 +0600

    module 22

--- Week-6/Module 22/InFix To PostFix/main.cpp
+++ Week-6/Module 22/InFix To PostFix/main.cpp
@@ -1,6 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
+int prec(char ch);
 int main()
 {
-    cout<<"Data is loading....";
+    string s;
+    cin>>s;
+    string ans="";
+    stack<char>st;
+    for(int i=0;i<s.size();i++)
+    {
+        char now = s[i];
+        if(now>='a' && now <= 'z')
+        {
+            ans +=now;
+        }
+        else
+        {
+            while(st.size() && prec(st.top()) >= prec(now))
+            {
+                ans += st.top();
+                st.pop();
+            }
+            st.push(now);
+        }
+    }
+    while(st.size())
+    {
+        ans += st.top();
+        st.pop();
+    }
+    cout<<ans;
+} 
+int prec(char ch)
+{
+    if(ch=='+' || ch== '-')
+    {
+        return 0;
+    }
+    
+    return 1;
 }
