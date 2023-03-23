3d869dcc15d16cf9d556c9c4a01c12b6dd5d41cc
MDalamin5 <mdal.amin5@northsouth.edu>
2023-03-23 17:53:58 +0600

    codeforce Beauty matrix difference

--- Conceptual Session/csWeek-04/Session 01/CodeForce/B.MatrixOfDifference/main.cpp
+++ Conceptual Session/csWeek-04/Session 01/CodeForce/B.MatrixOfDifference/main.cpp
@@ -1,15 +1,64 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int row;
-    cin>>row;
-    int arr[row][row];
-    for(int i=0;i<row;i++)
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int test;
+    cin>>test;
+    while(test--)
     {
-        for(auto j=0;j<row;j++)
+        int n;
+    cin>>n;
+    list<int>l;
+    for(int i=1;i<=(n*n);i++)
+    {
+        l.push_back(i);
+    }
+    int gird[n][n];
+    for(int i=0;i<n;i++)
+    {
+        if((i+1)%2!=0)
         {
-            cin>>arr[i][j];
+            for(int j=0;j<n;j++)
+            {
+                if((j+1)%2!=0)
+                {
+                    gird[i][j]=l.back();
+                    l.pop_back();
+                }
+                else
+                {
+                    gird[i][j]=l.front();
+                    l.pop_front();
+                }
+            }
         }
+        else
+        {
+            for(int j=n-1;j>=0;j--)
+            {
+                if((j+1)%2!=0)
+                {
+                    gird[i][j]=l.front();
+                    l.pop_front();
+                }
+                else
+                {
+                    gird[i][j]=l.back();
+                    l.pop_back();
+                }
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<gird[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     }
 }
