d389378f0f3ca41524ec68e9b96ea3fde3a56dd8
MDalamin5 <mdal.amin5@northsouth.edu>
2023-03-29 00:34:38 +0600

    26.5 Update

--- Week-7/Module 26.5 (Deque Practice)/Answer/Problem2.cpp
+++ Week-7/Module 26.5 (Deque Practice)/Answer/Problem2.cpp
@@ -1,23 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
+template<class T>
 class node
 {
     public:
-    int data;
-    node* prv;
-    node *next;
-    node(int data)
+    T data;
+    node<T> *prv;
+    node<T> *next;
+    node(T data)
     {
         this->data=data;
         this->prv=NULL;
         this->next=NULL;
     }
 };
+template<class T>
 class Deque
 {
     public:
-    node *head;
-    node *tail;
+    node<T> *head;
+    node<T> *tail;
     int size;
     Deque()
     {
@@ -25,9 +27,9 @@ class Deque
         tail=NULL;
         size=0;
     }
-    void pushFront(int data)
+    void pushFront(T data)
     {
-        node *newNode = new node(data);
+        node<T> *newNode = new node<T>(data);
         if(head==NULL)
         {
             head=newNode;
@@ -35,15 +37,15 @@ class Deque
             size++;
             return;
         }
-        node *temp= head;
+        node<T> *temp= head;
         newNode->next=head;
         temp->prv=newNode;
         head=newNode;
         size++;
     }
-    void pushBack(int data)
+    void pushBack(T data)
     {
-        node *newNode = new node(data);
+        node<T> *newNode = new node<T>(data);
         if(head==NULL)
         {
             head=newNode;
@@ -56,9 +58,73 @@ class Deque
         tail=newNode;
         size++;
     }
+    void popBack()
+    {
+        if(getSize()==0)
+        {
+            cout<<"Deque is Empty!!!!"<<endl;
+            return;
+        }
+        if(getSize()==1)
+        {
+            delete tail;
+            head=tail=NULL;
+            size--;
+            return;
+        }
+        node<T> *temp = tail;
+        tail=temp->prv;
+        tail->next=NULL;
+        delete temp;
+        size--;
+    }
+    void popFront()
+    {
+        if(getSize()==0)
+        {
+            cout<<"Deque is Empty!!!!"<<endl;
+            return;
+        }
+        if(getSize()==1)
+        {
+            delete tail;
+            head=tail=NULL;
+            size--;
+            return;
+        }
+        node<T> *temp = head;
+        head=temp->next;
+        head->prv=NULL;
+        delete temp;
+        size--;
+    }
+    T frontData()
+    {
+        if(getSize()==0)
+        {
+            cout<<"Deque is Empty!!!!"<<endl;
+            T ob;
+            return ob;
+        }
+        return head->data;
+    }
+    T backData()
+    {
+        if(getSize()==0)
+        {
+            cout<<"Deque is Empty!!!!"<<endl;
+            T ob;
+            return ob;
+        }
+        return tail->data;
+    }
+    int getSize()
+    {
+        return size;
+    }
     void travers()
     {
-        node *a = head;
+        node<T> *a = head;
         while(a!=NULL)
         {
             cout<<a->data<<" ";
@@ -69,11 +135,22 @@ class Deque
 };
 int main()
 {
-    Deque dq;
+    Deque<int> dq;
     dq.pushFront(1);
     dq.pushFront(4);
     dq.pushBack(55);
     dq.pushFront(3);
     dq.travers();
+    dq.popBack();
+    dq.travers();
+    dq.popFront();
+    dq.travers();
+    cout<<"Size of Deque: "<<dq.getSize()<<endl;
+    cout<<dq.frontData()<<endl;
+    cout<<dq.backData()<<endl;
+    
+    Deque<char> ch;
+    ch.pushBack('a');
+    ch.travers();
     return 0;
 }
