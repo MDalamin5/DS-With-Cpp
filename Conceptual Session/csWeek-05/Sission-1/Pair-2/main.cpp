1bfdb78dfdaaeac69e47cee85c5a6c809631c8cc
MDalamin5 <mdal.amin5@northsouth.edu>
2023-04-09 12:31:52 +0600

    Pair

--- Conceptual Session/csWeek-05/Sission-1/Pair-2/main.cpp
+++ Conceptual Session/csWeek-05/Sission-1/Pair-2/main.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main()
 {
+    /*
     vector<pair<string,int>>v;
     int n;
     cin>>n;
@@ -17,5 +18,28 @@ int main()
     for(int i=0;i<v.size();i++)
     {
         cout<<v[i].first<<" "<<v[i].second<<endl;
+    }
+    */
+    int n;
+    cout<<"Enter The value of n: ";
+    cin>>n;
+    vector<pair<pair<string,string>,int>> v;
+    cout<<"Enter The User info.."<<endl;
+    
+    //v.push_back({{"Md","Al Amin"},24});
+    for(int i=0;i<n;i++)
+    {
+        string firstName,lastName;
+        int age;
+        cout<<"Enter user first Name: ";
+        cin>>firstName>>lastName;
+        cout<<"Enter Age: ";
+        cin>>age;
+        v.push_back({{firstName,lastName},age});
+    }
+    for(int i=0;i<n;i++)
+    {
+        cout<<v[i].first.first<<" "<<v[i].first.second<<endl;
+        cout<<v[i].second<<endl;
     }
 }
