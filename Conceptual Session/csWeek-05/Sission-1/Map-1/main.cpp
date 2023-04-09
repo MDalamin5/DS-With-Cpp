4d4c5c55a7533476360d59c3a898ba40ac8e7b29
MDalamin5 <mdal.amin5@northsouth.edu>
2023-04-09 13:32:58 +0600

    Map

---
+++ Conceptual Session/csWeek-05/Sission-1/Map-1/main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    map<int ,int>mp;
+    mp[1]=4;
+    mp[1]=3;
+    mp[2]=55;
+    mp[11]=77;
+    for(auto it:mp)
+    {
+        cout<<it.first<< " "<<it.second<<endl;
+    }
+}
---
+++ Conceptual Session/csWeek-05/Sission-1/Pair-3/main.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<vector<int>>v(n);
+    v[0].push_back(10);
+    v[1].push_back(30);
+    cout<<v[0][0];
+    
+}
